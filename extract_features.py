8eb225165e1e970d4a7ed825a9bb4c6088c8f53b
sontung <tung.nguyenson1404@gmail.com>
2022-01-05 09:19:24 +0700

    extract2

--- extract_features.py
+++ extract_features.py
@@ -8,7 +8,6 @@ import torch
 
 from tqdm import tqdm
 
-import scipy
 import scipy.io
 import scipy.misc
 import cv2
@@ -17,75 +16,218 @@ from lib.utils import preprocess_image
 from lib.pyramid import process_multiscale
 from PIL import Image
 
-# CUDA
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda:0" if use_cuda else "cpu")
 
-# Argument parsing
-parser = argparse.ArgumentParser(description='Feature extraction script')
+def run_extraction(keypoint_only=False):
+    # CUDA
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
 
-parser.add_argument(
-    '--image_list_file', type=str, required=True,
-    help='path to a file containing a list of images to process'
-)
+    # Argument parsing
+    parser = argparse.ArgumentParser(description='Feature extraction script')
 
-parser.add_argument(
-    '--preprocessing', type=str, default='caffe',
-    help='image preprocessing (caffe or torch)'
-)
-parser.add_argument(
-    '--model_file', type=str, default='models/d2_tf.pth',
-    help='path to the full model'
-)
+    parser.add_argument(
+        '--image_list_file', type=str, required=True,
+        help='path to a file containing a list of images to process'
+    )
 
-parser.add_argument(
-    '--max_edge', type=int, default=1600,
-    help='maximum image size at network input'
-)
-parser.add_argument(
-    '--max_sum_edges', type=int, default=2800,
-    help='maximum sum of image sizes at network input'
-)
+    parser.add_argument(
+        '--preprocessing', type=str, default='caffe',
+        help='image preprocessing (caffe or torch)'
+    )
+    parser.add_argument(
+        '--model_file', type=str, default='models/d2_tf.pth',
+        help='path to the full model'
+    )
 
-parser.add_argument(
-    '--output_extension', type=str, default='.d2-net',
-    help='extension for the output'
-)
-parser.add_argument(
-    '--output_type', type=str, default='npz',
-    help='output file type (npz or mat)'
-)
+    parser.add_argument(
+        '--max_edge', type=int, default=1600,
+        help='maximum image size at network input'
+    )
+    parser.add_argument(
+        '--max_sum_edges', type=int, default=2800,
+        help='maximum sum of image sizes at network input'
+    )
 
-parser.add_argument(
-    '--multiscale', dest='multiscale', action='store_true',
-    help='extract multiscale features'
-)
-parser.set_defaults(multiscale=False)
+    parser.add_argument(
+        '--output_extension', type=str, default='.d2-net',
+        help='extension for the output'
+    )
+    parser.add_argument(
+        '--output_type', type=str, default='npz',
+        help='output file type (npz or mat)'
+    )
 
-parser.add_argument(
-    '--no-relu', dest='use_relu', action='store_false',
-    help='remove ReLU after the dense feature extraction module'
-)
-parser.set_defaults(use_relu=True)
+    parser.add_argument(
+        '--multiscale', dest='multiscale', action='store_true',
+        help='extract multiscale features'
+    )
+    parser.set_defaults(multiscale=False)
 
-args = parser.parse_args()
+    parser.add_argument(
+        '--no-relu', dest='use_relu', action='store_false',
+        help='remove ReLU after the dense feature extraction module'
+    )
+    parser.set_defaults(use_relu=True)
 
-print(args)
+    args = parser.parse_args()
 
-# Creating CNN model
-model = D2Net(
-    model_file=args.model_file,
-    use_relu=args.use_relu,
-    use_cuda=use_cuda
-)
+    # Creating CNN model
+    model = D2Net(
+        model_file=args.model_file,
+        use_relu=args.use_relu,
+        use_cuda=use_cuda
+    )
 
-# Process the file
-lines = ["/home/sontung/work/d2-net/qualitative/images/pair_1/2.jpg"]
-for line in tqdm(lines, total=len(lines)):
-    path = line.strip()
-    print(path)
+    # Process the file
+    lines = ["/home/sontung/work/d2-net/qualitative/images/pair_1/2.jpg"]
+    for line in tqdm(lines, total=len(lines)):
+        path = line.strip()
 
-    image = imageio.imread(path)
+        image = imageio.imread(path)
+        if len(image.shape) == 2:
+            image = image[:, :, np.newaxis]
+            image = np.repeat(image, 3, -1)
+
+        # TODO: switch to PIL.Image due to deprecation of scipy.misc.imresize.
+        resized_image = image
+        if max(resized_image.shape) > args.max_edge:
+            print("resize")
+            pil_img = Image.fromarray(resized_image)
+            scale = args.max_edge / max(resized_image.shape)
+            pil_img = pil_img.resize((int(resized_image.shape[1]*scale),
+                                      int(resized_image.shape[0]*scale)))
+            resized_image = np.array(pil_img).astype("float")
+
+        if sum(resized_image.shape[: 2]) > args.max_sum_edges:
+            resized_image = scipy.misc.imresize(
+                resized_image,
+                args.max_sum_edges / sum(resized_image.shape[: 2])
+            ).astype('float')
+
+        fact_i = image.shape[0] / resized_image.shape[0]
+        fact_j = image.shape[1] / resized_image.shape[1]
+
+        input_image = preprocess_image(
+            resized_image,
+            preprocessing=args.preprocessing
+        )
+
+        with torch.no_grad():
+            if args.multiscale:
+                keypoints, scores, descriptors = process_multiscale(
+                    torch.tensor(
+                        input_image[np.newaxis, :, :, :].astype(np.float32),
+                        device=device
+                    ),
+                    model
+                )
+            else:
+                keypoints, scores, descriptors = process_multiscale(
+                    torch.tensor(
+                        input_image[np.newaxis, :, :, :].astype(np.float32),
+                        device=device
+                    ),
+                    model,
+                    scales=[1]
+                )
+
+        # Input image coordinates
+        keypoints[:, 0] *= fact_i
+        keypoints[:, 1] *= fact_j
+        # i, j -> u, v
+        keypoints = keypoints[:, [1, 0, 2]]
+
+        if keypoint_only:
+            return keypoints
+
+        # img = resized_image.astype("uint8")
+        # for x, y, _ in keypoints:
+        #     x, y = map(int, (x, y))
+        #     # cv2.circle(img, (y, x), 5, (0, 0, 255), -1)
+        #     cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+        # img = cv2.resize(img, (img.shape[1]//2, img.shape[0]//2))
+        # cv2.imshow("t", img)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
+        if args.output_type == 'npz':
+            with open(path + args.output_extension, 'wb') as output_file:
+                np.savez(
+                    output_file,
+                    keypoints=keypoints,
+                    scores=scores,
+                    descriptors=descriptors
+                )
+        elif args.output_type == 'mat':
+            with open(path + args.output_extension, 'wb') as output_file:
+                scipy.io.savemat(
+                    output_file,
+                    {
+                        'keypoints': keypoints,
+                        'scores': scores,
+                        'descriptors': descriptors
+                    }
+                )
+        else:
+            raise ValueError('Unknown output type.')
+
+
+def extract_using_d2_net(image):
+    # CUDA
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+
+    # Argument parsing
+    parser = argparse.ArgumentParser(description='Feature extraction script')
+
+    parser.add_argument(
+        '--preprocessing', type=str, default='caffe',
+        help='image preprocessing (caffe or torch)'
+    )
+    parser.add_argument(
+        '--model_file', type=str, default='d2-net/models/d2_tf.pth',
+        help='path to the full model'
+    )
+
+    parser.add_argument(
+        '--max_edge', type=int, default=1600,
+        help='maximum image size at network input'
+    )
+    parser.add_argument(
+        '--max_sum_edges', type=int, default=2800,
+        help='maximum sum of image sizes at network input'
+    )
+
+    parser.add_argument(
+        '--output_extension', type=str, default='.d2-net',
+        help='extension for the output'
+    )
+    parser.add_argument(
+        '--output_type', type=str, default='npz',
+        help='output file type (npz or mat)'
+    )
+
+    parser.add_argument(
+        '--multiscale', dest='multiscale', action='store_true',
+        help='extract multiscale features'
+    )
+    parser.set_defaults(multiscale=False)
+
+    parser.add_argument(
+        '--no-relu', dest='use_relu', action='store_false',
+        help='remove ReLU after the dense feature extraction module'
+    )
+    parser.set_defaults(use_relu=True)
+
+    args = parser.parse_args()
+
+    # Creating CNN model
+    model = D2Net(
+        model_file=args.model_file,
+        use_relu=args.use_relu,
+        use_cuda=use_cuda
+    )
+
     if len(image.shape) == 2:
         image = image[:, :, np.newaxis]
         image = np.repeat(image, 3, -1)
@@ -93,18 +235,18 @@ for line in tqdm(lines, total=len(lines)):
     # TODO: switch to PIL.Image due to deprecation of scipy.misc.imresize.
     resized_image = image
     if max(resized_image.shape) > args.max_edge:
-        print("resize")
-        pil_img = Image.fromarray(resized_image)
         scale = args.max_edge / max(resized_image.shape)
-        pil_img = pil_img.resize((int(resized_image.shape[1]*scale),
-                                  int(resized_image.shape[0]*scale)))
+        pil_img = Image.fromarray(resized_image)
+        pil_img = pil_img.resize((int(resized_image.shape[1] * scale),
+                                  int(resized_image.shape[0] * scale)))
         resized_image = np.array(pil_img).astype("float")
 
     if sum(resized_image.shape[: 2]) > args.max_sum_edges:
-        resized_image = scipy.misc.imresize(
-            resized_image,
-            args.max_sum_edges / sum(resized_image.shape[: 2])
-        ).astype('float')
+        scale = args.max_sum_edges / sum(resized_image.shape[: 2])
+        pil_img = Image.fromarray(resized_image)
+        pil_img = pil_img.resize((int(resized_image.shape[1] * scale),
+                                  int(resized_image.shape[0] * scale)))
+        resized_image = np.array(pil_img).astype("float")
 
     fact_i = image.shape[0] / resized_image.shape[0]
     fact_j = image.shape[1] / resized_image.shape[1]
@@ -138,34 +280,4 @@ for line in tqdm(lines, total=len(lines)):
     keypoints[:, 1] *= fact_j
     # i, j -> u, v
     keypoints = keypoints[:, [1, 0, 2]]
-
-    img = resized_image.astype("uint8")
-    for x, y, _ in keypoints:
-        x, y = map(int, (x, y))
-        # cv2.circle(img, (y, x), 5, (0, 0, 255), -1)
-        cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
-    img = cv2.resize(img, (img.shape[1]//2, img.shape[0]//2))
-    cv2.imshow("t", img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-    if args.output_type == 'npz':
-        with open(path + args.output_extension, 'wb') as output_file:
-            np.savez(
-                output_file,
-                keypoints=keypoints,
-                scores=scores,
-                descriptors=descriptors
-            )
-    elif args.output_type == 'mat':
-        with open(path + args.output_extension, 'wb') as output_file:
-            scipy.io.savemat(
-                output_file,
-                {
-                    'keypoints': keypoints,
-                    'scores': scores,
-                    'descriptors': descriptors
-                }
-            )
-    else:
-        raise ValueError('Unknown output type.')
+    return keypoints
